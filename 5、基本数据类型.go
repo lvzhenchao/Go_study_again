b18e722d498d856c352cd90f72d9f92594b66436
lvzhenchao <1370164516@qq.com>
2021-11-01 22:43:47 +0800

    Update 5、基本数据类型.go

--- 5、基本数据类型.go
+++ 5、基本数据类型.go
@@ -13,8 +13,32 @@ func main()  {
 	fmt.Println(a)
 	var b float64 = 345.6
 	fmt.Println(b)
-	
+
 	b = float64(a)//类型转换
 	fmt.Println(b)
+
+	var score1 float32 = 98.9
+	var score2 float64 = 89.8
+	sum := score1 + float32(score2)
+	fmt.Println(score1, score2, sum)//浮点值的加减法 后面会加上不确定位
+	fmt.Println("----------")
+
+
+	//字符类型
+	var ch byte	//声明字符类型，关键字byte
+	ch = 'a'	//单引号，字符;字符类型用单引号括起来，【只能写一个字符】
+	fmt.Printf("%c", ch)
+	fmt.Println()
+	fmt.Println(ch)//ascll码 97
+	fmt.Println('b')//98
+	fmt.Println("----------")
+
+	//字符串类型
+	//【双引号】括起来的字符串类型
+	var str1 string
+	str1 = "abc"
+	fmt.Println("str = ", str1)
+	fmt.Printf("str1=%s", str1)
+	fmt.Println()
 
 }
