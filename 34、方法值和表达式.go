02697b4cf59a8cf534b6bf2601913a55eaac914d
lvzhenchao <1370164516@qq.com>
2021-11-11 22:15:08 +0800

    Create 34、方法值和表达式.go
    用的少，但是需要能看懂

---
+++ 34、方法值和表达式.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+//var dog Dog
+//掉用  dog.Bark()
+
+//方法值调用： var dog Dog
+//dfunc := dog.Bark //保存方法地址
+//dfunc()
+
+type operate struct {
+	num1 int
+	num2 int
+}
+
+func (o operate)Add()int   {
+	return o.num1 + o.num2
+}
+func (o *operate)Sub()int  {
+	return o.num1-o.num2
+}
+func main()  {
+
+	fmt.Println("===一般方法的调用====")
+	o:= operate{}
+	o.num1 = 456
+	o.num2 = 123
+	fmt.Println(o.Add(), o.Sub())
+
+	fmt.Println("===值方法的调用===")
+	f1 := o.Add
+	f2 := o.Sub
+	fmt.Println(f1,f2,f1(),f2())//0x6d7c40 0x6d7c80 579 333
+
+	fmt.Println("===表达式形式的调用===")
+	f3 := operate.Add
+	res1 := f3(o)
+
+	f4 := (*operate).Sub
+	res2 := f4(&o)
+	fmt.Println(res1, res2)
+}
