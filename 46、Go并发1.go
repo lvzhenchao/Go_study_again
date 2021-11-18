4919d974c2c80bd5db6b6609fa6bf94551ae25cf
lvzhenchao <1370164516@qq.com>
2021-11-18 22:35:26 +0800

    Create 46、Go并发1.go

---
+++ 46、Go并发1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//协程比线程小，10几个协程可能体现在底层就是5、6个线程
+//特性：主协程退出后，其他子协程也会自动退出
+
+func main()  {
+	fmt.Println("===协程与一般函数的使用===")
+	go running()
+
+	for i:=0; i<10; i++ {
+		fmt.Println("主进程执行：", i)
+		time.Sleep(time.Second*1)
+	}
+
+	fmt.Println("===协程和匿名函数的结合===")
+	flag := false//标志位
+	go func() {
+		for i:=0; i<10; i++ {
+			fmt.Println("匿名函数执行的并发：", i)
+			time.Sleep(time.Second*1)
+		}
+		flag = true//循环完之后更改值
+	}()
+
+	for {
+		if flag {
+			break
+		}
+	}
+}
+
+func running()  {//如果主程序（主goroutine）退出，并发的goroutine也会退出。【并发程序是寄宿在主程序中的】
+	for j:=0; j<100; j++ {
+		fmt.Println("子进程执行...", j)
+		time.Sleep(time.Second*1)
+	}
+}
