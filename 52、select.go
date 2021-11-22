38e04f0c6916fc2f282e8051e71e99394991c585
lvzhenchao <1370164516@qq.com>
2021-11-22 22:47:33 +0800

    Update 52、select.go

--- 52、select.go
+++ 52、select.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //关键字select，监听channel上的数据流动
 //限制：最大限制：每个case语句里必须是一个IO操作
@@ -9,15 +12,76 @@ import "fmt"
 //基本应用：超时
 
 func main()  {
-	ch := make(chan int, 1)
-	for {
+	//ch := make(chan int, 1)
+	//for {
+	//	select {
+	//	case ch <- 0:
+	//	case ch <- 1:
+	//	}
+	//	i := <-ch
+	//	fmt.Println("Value received:", i)
+	//}
+
+	fmt.Println("===select的基本使用===")
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second*3)
+		c1 <-"one"
+	}()
+	go func() {
+		time.Sleep(time.Second*2)
+		c2<-"two"
+	}()
+	for i:=0; i<2; i++ {
 		select {
-		case ch <- 0:
-		case ch <- 1:
+			case msg:=<-c1:
+				fmt.Println("接收到的数据为：", msg)
+			case msg1:=<-c2:
+				fmt.Println("接收到的数据为：",msg1)
 		}
-		i := <-ch
-		fmt.Println("Value received:", i)
+		fmt.Println("**********")
 	}
+
+	fmt.Println("===超时操作===")
+	TimeOutOperation()
+
+
 }
+
+func TimeOutOperation()  {
+	c := make(chan int)
+	out := make(chan bool)
+	go func() {
+		for{
+			select {
+			case v:=<-c:
+				fmt.Println(v)
+			case <-time.After(time.Second*5):
+				fmt.Println("超时！")
+				out<-true//标志位
+				return//退出
+
+			}
+		}
+	}()
+
+	//主进程
+	<-out//一直等待状态，标志位为阻塞状态
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
