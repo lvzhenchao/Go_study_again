f0adba656b4f69ade3d1c60c021b0ba0fa31bcb3
lvzhenchao <1370164516@qq.com>
2021-11-23 16:59:13 +0800

    Update 27、指针.go

--- 27、指针.go
+++ 27、指针.go
@@ -5,6 +5,11 @@ import "fmt"
 //指针就是地址【虚拟地址】；
 //指针变量：存放地址的变量 new(type)  例如：不同类型的指针 *int *string *bool
 
+//Go语言中的 new 和 make 主要区别如下：
+	//make 只能用来分配及初始化类型为 slice、map、chan 的数据。new 可以分配任意类型的数据；
+	//new 分配返回的是指针，即类型 *Type。make 返回引用，即 Type；
+	//new 分配的空间被清零。make 分配空间后，会进行初始化。
+
 type student struct {
 	id int
 	name string
