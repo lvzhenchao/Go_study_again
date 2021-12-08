32a10de40dec00c329d52f118315c32fad923cf8
lvzhenchao <1370164516@qq.com>
2021-12-08 16:04:58 +0800

    混合类型不定参数实例

--- 22、不定参数.go
+++ 22、不定参数.go
@@ -23,12 +23,22 @@ func main()  {
 	s := make([]string,0)
 	s = append(s, "ss")
 	params1(s...)//记得加3个小点儿
+
+	fmt.Println("---混合类型不定参数---")
+	mixType := make([]interface{},5)
+	mixType = append(mixType, "mix1", 23, true,12.3)
+	params2(mixType...)
 }
 
 
 func params1(args...string)  {
 	for k,v := range args {
 		fmt.Println(k,v)
+	}
+}
+func params2(args...interface{})  {
+	for k,v := range args {
+		fmt.Printf("key:%v, value类型：%\n" ,k,v)
 	}
 }
 
